b7ab6bf120a757ddb0a4c96c015ca07cddf0d4ee
Clayton Davidson (1000061387) <DAVICA3@student.op.ac.nz>
2019-05-08 12:53:23 +1200

    Adding project to Git Hub

---
+++ PrimeGenerator/PrimeGenerator/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace PrimeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            do
+            {
+                Console.Write("______Prime Number Application______\n\n[1] Prime Checker\n[2] Prime Finder\n[3] Close\n\nEnter Choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        PrimeCheck();
+                        break;
+                    case 2:
+                        PrimeFind();
+                        break;
+                }
+            } while (choice != 3);
+        }
+        
+        static void PrimeCheck()
+        {
+            bool prime = true;
+            
+            Console.Write("Enter a number: ");
+            BigInteger user = BigInteger.Parse(Console.ReadLine());
+
+            for (BigInteger i = 2; Convert.ToDouble(i) <= Math.Exp(BigInteger.Log(user) / 2); i++)
+            {
+                if ((user % i) == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime == true)
+            {
+                Console.WriteLine($"{user} is a prime number\n");
+            }
+            else
+            {
+                Console.WriteLine("Not prime\n");
+            }
+        }
+
+        static void PrimeFind()
+        {
+            BigInteger num = 1;
+            string stringNum = Convert.ToString(num);
+            bool prime = true;
+
+            Console.Write("Enter the length in digits (MAX OF 16): ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            BigInteger value = BigInteger.Pow(10, limit-1)/4;
+
+            if (File.Exists(@"H:\temp.txt"))
+            {
+                File.Delete(@"H:\temp.txt");
+                File.Create(@"H:\temp.txt").Close();
+                File.AppendAllText(@"H:\temp.txt", value.ToString());
+            }
+            else
+            {
+                File.Create(@"H:\temp.txt").Close();
+                File.AppendAllText(@"H:\temp.txt", value.ToString());
+            }
+
+            Process.Start(@"H:\My Documents\Course_Work\IN510_Programming_1\Window1\Window1\bin\Debug\Window1.exe");
+            Process.Start(@"H:\My Documents\Course_Work\IN510_Programming_1\Window2\Window2\bin\Debug\Window2.exe");
+            Process.Start(@"H:\My Documents\Course_Work\IN510_Programming_1\Window3\Window3\bin\Debug\Window3.exe");
+
+            while ((num < value) || (prime == false))
+            {
+                prime = true;
+
+                for (int i = 2; i <= BigInteger.Pow(num, (1/2)); i++)
+                {
+                    if ((num % i) == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                if (prime == true)
+                {
+                    Console.WriteLine(num);
+                }
+
+                if (num + 1 < value)
+                {
+                    num++;
+                }
+                else
+                {
+                    break;
+                }
+                stringNum = Convert.ToString(num);
+                
+            }
+        }
+    }
+}
---
+++ Window3/Window3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Window3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prime = true;
+	        string[] temp = File.ReadAllLines(@"H:\temp.txt");
+            long value = Convert.ToInt64(temp[0]);
+            long num = value*3;
+            string stringNum = Convert.ToString(num);
+
+            while ((num < value * 4) || (prime == false))
+            {
+                prime = true;
+
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if ((num % i) == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                if (prime == true)
+                {
+                    Console.WriteLine(num);
+                }
+
+                if (num + 1 < value * 4)
+                {
+                    num++;
+                }
+                else
+                {
+                    break;
+                }
+                stringNum = Convert.ToString(num);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
